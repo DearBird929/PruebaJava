38e722240f38e0c2c1cd98d004ffea3ae07b6457
SergioCM-DEV <sca49224@iesvirreymorcillo.es>
2025-01-20 09:34:55 +0100

    2º Cambio (Ejercicio 3 aun no terminado) 2.0

---
+++ Ejercicio3.java
@@ -0,0 +1,24 @@
+/* Hacer un programa que pida 6 números decimales al usuario
+ * y los guarde en un array, después debe mostrarlos en orden 
+ * que se introdujeron.
+ */
+import java.util.Scanner;
+public class Ejercicio3 {
+    Scanner leerDatos = new Scanner(System.in);
+    double[] numeros = new double[10];
+    int negativos = 0;
+    int positivos = 0;
+    int ceros = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        System.out.println("\nIntroduce los 10 números:");
+    }
+
+    if (i <0) {
+        negativos++;
+    } if else (i > 0) {
+        positivos++;
+    } else {
+        ceros++;
+    }
+}
