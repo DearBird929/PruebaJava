c7e888b20c8f0491023cbf76df01d0209bc14331
SergioCM-DEV <sca49224@iesvirreymorcillo.es>
2025-01-21 12:01:56 +0100

    un par de cambios despues del problema con java23

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -14,4 +14,5 @@ public static void main (String[] args) {
     for (int i = 0; i < 6; i++) {
         System.out.println(numeros[i]);
     }
+    leerDatos.close();
 }
--- Ejercicio3.java
+++ Ejercicio3.java
@@ -21,4 +21,5 @@ public class Ejercicio3 {
     } else {
         ceros++;
     }
+    leerDatos.close();
 }
